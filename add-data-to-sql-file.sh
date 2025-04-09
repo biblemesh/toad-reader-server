93bd26689c4ad922458d9b78553508338c944633
Joel Cross <ukch@users.noreply.github.com>
2025-04-09 18:28:15 +0100

    Use GNU envsubst and alpine

--- add-data-to-sql-file.sh
+++ add-data-to-sql-file.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ENVSUBST="envsubst -no-unset -no-empty"
+ENVSUBST="envsubst"
 
 INPUT="./seeds.sql"
 OUTPUT="./output/02-initdata.sql"
@@ -17,4 +17,4 @@ export SAML_SERVICE_PROVIDER_EREADER_ENCRYPTION_CERTIFICATE=$(cat sp-read-encryp
 export SAML_SERVICE_PROVIDER_EREADER_ENCRYPTION_PRIVATE_KEY=$(cat sp-read-encrypt.key | sed --regexp-extended '/\-+(BEGIN|END) PRIVATE KEY\-+[[:space:]]*/d' | sed ':a;N;$!ba;s/\n/\\n/g')
 export SHIBBOLETH_IDENTITY_PROVIDER_SIGNING_CERTIFICATE=$(cat idp-signing.crt | sed --regexp-extended '/\-+(BEGIN|END) CERTIFICATE\-+[[:space:]]*/d' | sed ':a;N;$!ba;s/\n/\\n/g')
 
-$ENVSUBST -i "$INPUT" -o "$OUTPUT"
+$ENVSUBST < "$INPUT" > "$OUTPUT"
