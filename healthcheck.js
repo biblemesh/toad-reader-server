c411cd886655e77956f3b52493cb2cffbed6f10c
Trevor Dearham <trevor.dearham@gmail.com>
2025-07-07 14:22:24 +0100

    Handle IPv4 and IPv6 addresses

--- healthcheck.js
+++ healthcheck.js
@@ -3,14 +3,16 @@
 const http = require('http');
 
 http
-  .get(`http://127.0.0.1:${parseInt(process.env.PORT, 10) || process.env.PORT || 8080}/Shibboleth.sso/Metadata`, (res) => {
-    console.log('statusCode', res.statusCode);
-    if (res.statusCode === 200) {
-      process.exit(0);
-    } else {
-      process.exit(1);
-    }
-  })
+  .get(`http://localhost:${process.env.PORT || 8080}/Shibboleth.sso/Metadata`,
+    { host: `${process.env.DEFAULT_IDP_DOMAIN || 'localhost'}:${process.env.PORT || 8080}` },
+    (res) => {
+      console.log('statusCode', res.statusCode);
+      if (res.statusCode === 200) {
+        process.exit(0);
+      } else {
+        process.exit(1);
+      }
+    })
   .on('error', (err) => {
     console.log('error', err);
     process.exit(1);
