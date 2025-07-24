0b681cb555d260e1f8e1b1b4b5f669ea88bd5f42
Joel Cross <ukch@users.noreply.github.com>
2025-07-24 15:12:24 +0300

    Fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,18 +7,22 @@ import tseslint from 'typescript-eslint';
 global.structuredClone = (val) => JSON.parse(JSON.stringify(val))  // eslint-disable-line no-undef
 
 export default tseslint.config([
+  pluginJs.configs.recommended, // Apply recommended ESLint rules
+  tseslint.configs.recommended, // Apply recommended TypeScript rules
+  prettierConfig, // Disable rules that conflict with Prettier
   {
     files: ['**/*.js'],
     languageOptions: {
       sourceType: 'commonjs',
       globals: globals.node, // Enable Node.js global variables
     },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-redeclare': 'off',
+    },
   },
   {
     files: ['**/*.mjs'],
     languageOptions: { sourceType: 'module' },
   },
-  pluginJs.configs.recommended, // Apply recommended ESLint rules
-  tseslint.configs.recommended, // Apply recommended TypeScript rules
-  prettierConfig, // Disable rules that conflict with Prettier
 ]);
