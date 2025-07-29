40db96ece3f27d8aebbe603eed91f7dae414e616
Trevor Dearham <trevor.dearham@gmail.com>
2025-07-29 10:33:11 +0100

    Can't use host to select IDP domain

--- healthcheck.js
+++ healthcheck.js
@@ -6,7 +6,6 @@ const { log } = require('./src/utils/logger')
 
 http
   .get(`http://localhost:${process.env.PORT || 8080}/Shibboleth.sso/Metadata`,
-    { host: `${process.env.DEFAULT_IDP_DOMAIN || 'localhost'}:${process.env.PORT || 8080}` },
     (res) => {
       log(['statusCode', res.statusCode]);
       if ([200, 302, 304].includes(res.statusCode)) {
