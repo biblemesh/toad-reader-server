860b88df79071f7846b5626b7a10eab8048c9752
Joel Cross <ukch@users.noreply.github.com>
2025-12-23 14:46:59 -0600

    Node versions are now [18, 20]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,6 @@ import pluginJs from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-// This is needed until we upgrade to Node 18.
-global.structuredClone = (val) => JSON.parse(JSON.stringify(val)); // eslint-disable-line no-undef
-
 export default tseslint.config([
   pluginJs.configs.recommended, // Apply recommended ESLint rules
   tseslint.configs.recommended, // Apply recommended TypeScript rules
