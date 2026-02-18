c2053c2051d1a712698282f15e8e6589250c84f0
Trevor Dearham <trevor.dearham@gmail.com>
2026-02-18 17:03:35 +0000

    Ensure cron tasks runs even if startup delayed

--- crons/run.js
+++ crons/run.js
@@ -1,6 +1,6 @@
 const { handler } = require('../crons');
 
 (async () => {
-  await handler();
+  await handler({ forceRunAll: true }); // Ensure cron tasks are run even if there is a delay in startup
   process.exit();
 })();
