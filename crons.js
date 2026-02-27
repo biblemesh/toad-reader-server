7016cd2c91f46e47d9a57dc51a05e6ea105e613f
Trevor Dearham <trevor.dearham@gmail.com>
2026-02-27 16:56:41 +0000

    Formatting

--- crons.js
+++ crons.js
@@ -1,34 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const { log } = require('./src/utils/logger')
-const util = require('./src/utils/util')
-const dueDateReminders = require('./crons/due_date_reminders')
+const { log } = require('./src/utils/logger');
+const util = require('./src/utils/util');
+const dueDateReminders = require('./crons/due_date_reminders');
 // const xapiPosts = require('./crons/xapi_posts')
 
-const next = err => {
-  if(err) {
-    log(err, 3)
+const next = (err) => {
+  if (err) {
+    log(err, 3);
   }
-}
+};
 
-const crons = async ({ forceRunAll }={}) => {
-  await util.getValidConnection()
+const crons = async ({ forceRunAll } = {}) => {
+  await util.getValidConnection();
 
   //const day = new Date().getDay()  // 0-6
-  const hours = new Date().getHours()  // 0-23
-  const minutes = new Date().getMinutes()  // 0-59
+  const hours = new Date().getHours(); // 0-23
+  const minutes = new Date().getMinutes(); // 0-59
 
   // every minute
   // await xapiPosts({ next })  // commented out since (1) code was moved around and has not yet been tested since then, and (2) no tenant currently uses this
 
-  if((minutes === 0) || forceRunAll) {  // once per hour
-    await dueDateReminders({ next })
+  if (minutes === 0 || forceRunAll) {
+    // once per hour
+    await dueDateReminders({ next });
   }
 
-  if((hours === 0 && minutes === 0) || forceRunAll) {  // once per day
-
+  if ((hours === 0 && minutes === 0) || forceRunAll) {
+    // once per day
   }
+};
 
-}
-
-module.exports.handler = crons
+module.exports.handler = crons;
