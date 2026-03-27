2c9b21785aa552acdfc2b8988f966732fc5f300f
Joel Cross <ukch@users.noreply.github.com>
2026-03-26 23:41:15 -0600

    Format

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,10 @@
 const logger = {
   log: (msgs, importanceLevel) => {
-    const logLevel = parseInt(process.env.LOG_LEVEL) || 3   // 1=verbose, 2=important, 3=errors only
-    importanceLevel = importanceLevel || 1
+    const logLevel = parseInt(process.env.LOG_LEVEL) || 3; // 1=verbose, 2=important, 3=errors only
+    importanceLevel = importanceLevel || 1;
     if (importanceLevel >= logLevel) {
       if (!Array.isArray(msgs)) {
-        msgs = [msgs]
+        msgs = [msgs];
       }
       const timestamp = new Date().toLocaleString('en-US', {
         timeZone: 'UTC',
@@ -17,10 +17,10 @@ const logger = {
         second: '2-digit',
         hour12: false, // 24-hour format
       }); // Start logs with timestamp to enabled log lines to be detected https://docs.aws.amazon.com/batch/latest/userguide/using_awslogs.html#create_awslogs_logdriver_options
-      msgs.unshift(timestamp, ['LOG ', 'INFO', 'ERR '][importanceLevel - 1])
-      console.log.apply(this, msgs)
+      msgs.unshift(timestamp, ['LOG ', 'INFO', 'ERR '][importanceLevel - 1]);
+      console.log.apply(this, msgs);
     }
-  }
-}
+  },
+};
 
-module.exports = logger
+module.exports = logger;
